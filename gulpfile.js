8a9825ca7663fce5ee78c321966e05999edc98f5
Omer Tarik <omertarik96@gmail.com>
2018-06-17 12:20:00 -0500

    gulp file changed

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// TODO add build functionality
 var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 
@@ -7,8 +8,8 @@ gulp.task('default', ['watch']);
 // JsHint Task
 gulp.task('jshint', function(){
     return gulp.src('js/**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('watch', function(){
